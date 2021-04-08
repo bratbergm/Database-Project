960cbf6cd6d68b62f51d2a37b318ee02feafcb3c
morterb <morterb@stud.ntnu.no>
2021-04-08 19:47:18 +0200

    removed files

--- code/createJson.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'db/dbCredentials.php';
-require_once 'db/OrdersModel.php';
-
-$model = new OrderModel();
-//print($model->createOrdersDoc() . "\n");
-print($model->createOrdersItemsDoc() . "\n");
